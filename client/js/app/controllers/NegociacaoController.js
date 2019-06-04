e35ffc6c9540eb3ca16abe0d319b418fba1d1191
Everton Paiva <evertonpaiva@gmail.com>
2019-06-04 15:41:52 -0300

    Criando helper para manipular datas.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,23 +13,21 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
+        let helper = new DateHelper();
 
         let negociacao = new Negociacao(
-          data,
+          helper.textoParaData(this._inputData.value),
           this._inputQuantidade.value,
           this._inputValor.value
         );
+
 
         let form = document.querySelector('form');
         form.reset();
         this._inputData.focus();
 
         console.log(negociacao);
+        console.log(helper.dataParaTexto(negociacao.data));
         return negociacao;
     }
 }
