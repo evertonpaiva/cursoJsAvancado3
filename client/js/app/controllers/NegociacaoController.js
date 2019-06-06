60727f6c859bb6eae6adea6393db0bcd65df9c65
Everton Paiva <evertonpaiva@gmail.com>
2019-06-06 10:42:44 -0300

    API Reflection para trocar o contexto do this

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,10 +7,15 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
 
+        this._listaNegociacoes = new ListaNegociacoes(this, function(model) {
+
+            this._negociacoesView.update(model);
+        });
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
@@ -19,9 +24,7 @@ class NegociacaoController {
     adiciona(event) {
 
         event.preventDefault();
-
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociação adiconada com sucesso';
         this._mensagemView.update(this._mensagem);
@@ -32,7 +35,6 @@ class NegociacaoController {
     apaga() {
 
         this._listaNegociacoes.esvazia();
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
