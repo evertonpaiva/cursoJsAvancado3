0dcfb1f1df494f1a41debe245c47554c3ea0d895
Everton Paiva <evertonpaiva@gmail.com>
2019-06-10 10:34:08 -0300

    Evitando que o programador feche a conexão diretamente a partir do connection.

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,10 +1,12 @@
 var ConnectionFactory = (function (){
 
-    var stores = ['negociacoes'];
-    var version = 4;
-    var dbName = 'aluraframe';
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
 
     var connection = null;
+
+    var close = null;
 
     return class ConnectionFactory {
 
@@ -27,7 +29,13 @@ var ConnectionFactory = (function (){
 
                 openRequest.onsuccess = e => {
 
-                    if(!connection) connection = e.target.result;
+                    if(!connection) {
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                            throw new Error('Você não pode fechar diretamente a conexão');
+                        }
+                    }
                     resolve(connection);
                 };
 
@@ -47,6 +55,14 @@ var ConnectionFactory = (function (){
 
                 connection.createObjectStore(store, { autoIncrement: true});
             });
+        }
+
+        static closeConnection(){
+
+            if(connection) {
+                close();
+                connection = null;
+            }
         }
     }
 
