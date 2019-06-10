97ff94d77143a76fc9b67920ee6ec25f620eaad4
Everton Paiva <evertonpaiva@gmail.com>
2019-06-10 12:11:19 -0300

    Implementando a remoção de todas as negociações

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -28,7 +28,11 @@ class NegociacaoController {
             .then(dao => dao.listaTodos())
             .then(negociacoes =>
                 negociacoes.forEach(negociacao =>
-                    this._listaNegociacoes.adiciona(negociacao)));
+                    this._listaNegociacoes.adiciona(negociacao)))
+            .catch(erro => {
+                console.log(erro);
+                this._mensagem.texto = erro;
+            });
     }
 
     adiciona(event) {
@@ -66,8 +70,14 @@ class NegociacaoController {
 
     apaga() {
 
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();
+            });
     }
 
     _criaNegociacao(){
